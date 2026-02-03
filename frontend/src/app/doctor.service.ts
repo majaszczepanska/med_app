c9ba272d5af8b2d82298e0b7c0b79d7906a164e5
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-03 22:33:08 +0100

    create doctor service with basic functions

---
+++ frontend/src/app/doctor.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DoctorService {
+    private apiUrl = 'http://192.168.131.213:8080/doctors';
+    constructor(private http: HttpClient) { }
+
+    getDoctors(): Observable<any> {
+      return this.http.get(this.apiUrl);
+    }
+  
+    createDoctor(doctor: any): Observable<any> {
+      return this.http.post(this.apiUrl, doctor);
+    }
+  
+    /*
+    updateDoctor(id: number, doctor: any): Observable<any> {
+      return this.http.put(`${this.apiUrl}/${id}`, doctor);
+    }
+    */
+  
+    deleteDoctor(id: number): Observable<any> {
+      return this.http.delete(`${this.apiUrl}/${id}`);
+    }
+}
