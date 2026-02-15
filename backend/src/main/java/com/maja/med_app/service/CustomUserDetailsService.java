a0dd9af86df79400c1bd253aa00501013c023395
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-15 19:10:26 +0100

    delete old imports

--- backend/src/main/java/com/maja/med_app/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/maja/med_app/service/CustomUserDetailsService.java
@@ -5,8 +5,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.maja.med_app.model.AppUser;
 import com.maja.med_app.repository.UserRepository;
