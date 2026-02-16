9d5449f4c1faceaa0056854cbc58f0cc3b3721b9
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-16 21:37:10 +0100

    move handleErrors function from app component to error service

---
+++ frontend/src/app/services/error.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorService {
+
+  handleErrors(err: any) {
+    console.error(err);
+
+    if (err.error && typeof err.error === 'string') {
+      try {
+        err.error = JSON.parse(err.error);
+      } catch (e) {
+      }
+    }
+    if (err.error && err.error.message) {
+      alert("❌ ERROR: " + err.error.message);
+      return;
+    }
+    if (err.error && typeof err.error === 'object') {
+      let errorMessage = "❌ VALIDATION ERRORS:\n";
+      let hasValidationErrors = false;
+
+        for (const key in err.error) {
+          if (err.error.hasOwnProperty(key)) {
+            hasValidationErrors = true;
+            errorMessage += `\n• ${key.toUpperCase()}:\n`;
+            const fullErrorText = err.error[key];
+            let lines = fullErrorText.split(',');
+            lines = lines
+              .map((line: string) => line.trim())
+              .sort((a: string, b: string) => b.localeCompare(a));
+            lines.forEach((line: string) => {
+              errorMessage += `    • ${line}\n`;
+            });
+          } 
+        }
+        if (hasValidationErrors) {
+            alert(errorMessage);
+            return;
+        }
+    } 
+    if (typeof err.error === 'string') {
+      alert("❌ ERROR: " + err.error);
+      return;
+    } 
+    alert("❌ ERROR: " + (err.error || "Server error"));
+  }
+}
