79d5d7b82f6859bff494569f939e2b0c4feeec4a
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-17 13:34:14 +0100

    change booleans and lists to check if appointment status not cancelled

--- backend/src/main/java/com/maja/med_app/repository/AppointmentRepository.java
+++ backend/src/main/java/com/maja/med_app/repository/AppointmentRepository.java
@@ -7,18 +7,32 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.maja.med_app.model.Appointment;
+import com.maja.med_app.model.AppointmentStatus;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long>  {
-    List<Appointment> findAllByDeletedFalse();
-    //existBy (select count(*)>0), Doctor (in appointment look for doctor in doctor look for the id field), VisitTime (in appointment look for VisitTime field)
-    boolean existsByPatientIdAndVisitTimeAndDeletedFalse(Long patientId, LocalDateTime visitTime);
-    boolean existsByDoctorIdAndVisitTimeAndDeletedFalse(Long doctorId, LocalDateTime visitTime);
+    //List<Appointment> findAllByDeletedFalse();
+    List<Appointment> findAllByStatusNot(AppointmentStatus status);
+   
+    //boolean existsByPatientIdAndVisitTimeAndDeletedFalse(Long patientId, LocalDateTime visitTime);
+    boolean existsByPatientIdAndVisitTimeAndStatusNot(Long patientId, LocalDateTime visitTime, AppointmentStatus status);
+    //boolean existsByDoctorIdAndVisitTimeAndDeletedFalse(Long doctorId, LocalDateTime visitTime);
+    boolean existsByDoctorIdAndVisitTimeAndStatusNot(Long doctorId, LocalDateTime visitTime, AppointmentStatus status);
 
-    boolean existsByPatientIdAndVisitTimeAndDeletedFalseAndIdNot(Long patientId, LocalDateTime visitTime, Long appointmentId);
-    boolean existsByDoctorIdAndVisitTimeAndDeletedFalseAndIdNot(Long doctorId, LocalDateTime visitTime, Long appointmentId);
+    //boolean existsByPatientIdAndVisitTimeAfterAndDeletedFalse(Long patientId, LocalDateTime visitTime);
+    boolean existsByPatientIdAndVisitTimeAfterAndStatusNot(Long patientId, LocalDateTime visitTime, AppointmentStatus status);
+
+    //boolean existsByDoctorIdAndVisitTimeAfterAndDeletedFalse(Long doctorId, LocalDateTime visitTime);
+    boolean existsByDoctorIdAndVisitTimeAfterAndStatusNot(Long doctorId, LocalDateTime visitTime, AppointmentStatus status);
+
+    //boolean existsByPatientIdAndVisitTimeAndDeletedFalseAndIdNot(Long patientId, LocalDateTime visitTime, Long appointmentId);
+    boolean existsByPatientIdAndVisitTimeAndStatusNotAndIdNot(Long patientId, LocalDateTime visitTime, AppointmentStatus status, Long appointmentId);
+
+    //boolean existsByDoctorIdAndVisitTimeAndDeletedFalseAndIdNot(Long doctorId, LocalDateTime visitTime, Long appointmentId);
+    boolean existsByDoctorIdAndVisitTimeAndStatusNotAndIdNot(Long doctorId, LocalDateTime visitTime, AppointmentStatus status, Long appointmentId);
     
-    List<Appointment> findAllByDoctorIdAndVisitTimeBetweenAndDeletedFalse(Long doctorId, LocalDateTime start, LocalDateTime end);
+    //List<Appointment> findAllByDoctorIdAndVisitTimeBetweenAndDeletedFalse(Long doctorId, LocalDateTime start, LocalDateTime end);
+    List<Appointment> findAllByDoctorIdAndVisitTimeBetweenAndStatusNot(Long doctorId, LocalDateTime start, LocalDateTime end, AppointmentStatus status);
 
-    List<Appointment> findAllByPatientIdAndDeletedFalse(Long patientId);
+    List<Appointment> findAllByPatientIdAndStatusNot(Long patientId, AppointmentStatus status);
 }
