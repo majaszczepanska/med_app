760ea8df652741f2c2406e66fcdff4ce53defb60
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-22 17:02:25 +0100

    add new class for list of specialization

---
+++ backend/src/main/java/com/maja/med_app/model/Specialization.java
@@ -0,0 +1,14 @@
+package com.maja.med_app.model;
+
+public enum Specialization {
+    CARDIOLOGIST,
+    DERMATOLOGIST,
+    GENERAL_PRACTITIONER,
+    GYNECOLOGIST,
+    NEUROLOGIST,
+    ONCOLOGIST,
+    ORTHOPEDIST,
+    PEDIATRICIAN,
+    PSYCHIATRIST,
+    SURGEON
+}
