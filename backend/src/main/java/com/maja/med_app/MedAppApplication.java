b81259deedb52b80990e0258e89e44c1fd2c57b9
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-22 19:28:51 +0100

    enable async

--- backend/src/main/java/com/maja/med_app/MedAppApplication.java
+++ backend/src/main/java/com/maja/med_app/MedAppApplication.java
@@ -2,8 +2,10 @@ package com.maja.med_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MedAppApplication {
 
 	public static void main(String[] args) {
