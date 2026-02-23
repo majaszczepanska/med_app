7b35a13359bf50f120378249b4c47fcf622d768e
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-23 13:29:01 +0100

    add new search for users (by token)

--- backend/src/main/java/com/maja/med_app/repository/UserRepository.java
+++ backend/src/main/java/com/maja/med_app/repository/UserRepository.java
@@ -9,4 +9,5 @@ import com.maja.med_app.model.AppUser;
 public interface UserRepository extends JpaRepository<AppUser, Long> {
 
     Optional<AppUser> findByEmail(String email); 
+    Optional<AppUser> findByVerificationToken(String verificationToken);
 }
