fac4e7b28067fa0bdac1589c1702484d59b1887f
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-23 13:27:53 +0100

    add auth verify page to request matchers

--- backend/src/main/java/com/maja/med_app/config/SecurityConfig.java
+++ backend/src/main/java/com/maja/med_app/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
             //.cors(Customizer.withDefaults())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .requestMatchers("/auth/register", "/auth/login").permitAll()
+                .requestMatchers("/auth/register", "/auth/login", "/auth/verify").permitAll()
                 .anyRequest().authenticated()
                 //.anyRequest().permitAll()
             )
