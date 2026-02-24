16d81b3e9ebfd2c9d91ececf6378f9bdfc523644
Maja Szczepańska <maja.szczepanska06@gmail.com>
2026-02-24 14:19:11 +0100

    add path for reset and forgot password pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { LoginComponent } from './login/login';
 import { RegisterComponent } from './register/register';
 import { Profile } from './profile/profile';
 import { guestGuard } from './services/guest-guard';
+import { ForgotPassword } from './forgot-password/forgot-password';
+import { ResetPassword } from './reset-password/reset-password';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent, canActivate: [guestGuard]},
     {path: 'register', component: RegisterComponent, canActivate: [guestGuard]},
-    {path: 'profile', component: Profile, canActivate: [guestGuard]},
+    //{path: 'profile', component: Profile, canActivate: [guestGuard]},
+    {path: 'reset-password', component: ResetPassword, canActivate: [guestGuard]},
+    {path: 'forgot-password', component: ForgotPassword, canActivate: [guestGuard]},
     {path: 'dashboard', children: []},
     { path: '**', redirectTo: 'login', pathMatch: 'full' }
 ];
